6744ba23de21c65170f87befbaadc7df80a288d7
dannjb <deeja@users.noreply.github.com>
2025-07-02 20:36:01 +1200

    Formatting

--- test/test.cpp
+++ test/test.cpp
@@ -14,31 +14,27 @@ void tearDown()
 void test_completeName()
 {
     TEST_MESSAGE("Testing complete name...");
-    BtHomeV2Device btHome("DIY-sensor","DIY-sensor", false);
+    BtHomeV2Device btHome("DIY-sensor", "DIY-sensor", false);
     TEST_ASSERT_EQUAL_STRING("0201060B094449592D73656E736F720416D2FC40", btHome.getBytes().c_str());
 
     TEST_MESSAGE("Adding unexpected test name. Should not change the data");
-    BtHomeV2Device btHome2("test","DIY-sensor", false);
+    BtHomeV2Device btHome2("test", "DIY-sensor", false);
     TEST_ASSERT_EQUAL_STRING("0201060B094449592D73656E736F720416D2FC40", btHome2.getBytes().c_str());
-
-
-} 
-
-
+}
 
 void test_completeNameLengthReduction()
 {
     Serial.println("Testing BtHome full local name...");
-    const char* shortName = "bbb";
-    const char* completeName = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; // 30 characters
-    BtHomeV2Device btHome(shortName, completeName, false); // name is too long
+    const char *shortName = "bbb";
+    const char *completeName = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; // 30 characters
+    BtHomeV2Device btHome(shortName, completeName, false);      // name is too long
     TEST_ASSERT_EQUAL_STRING("020106150961616161616161616161616161616161616161610416D2FC40", btHome.getBytes().c_str());
 }
 
 void test_completeNameLengthSwitchover()
 {
     Serial.println("Testing BtHome full local name...");
-    BtHomeV2Device btHome("bbb","aaaaaaaaaaaa", false);
+    BtHomeV2Device btHome("bbb", "aaaaaaaaaaaa", false);
     TEST_ASSERT_EQUAL_STRING("0201060D096161616161616161616161610416D2FC40", btHome.getBytes().c_str());
     btHome.addCount_0_4294967295(1234567890);
     TEST_ASSERT_EQUAL_STRING("0201060D096161616161616161616161610916D2FC403ED2029649", btHome.getBytes().c_str());
@@ -48,11 +44,11 @@ void test_completeNameLengthSwitchover()
     TEST_ASSERT_EQUAL_STRING("02010604086262621316D2FC403ED20296493ED20296493ED2029649", btHome.getBytes().c_str());
     btHome.addCount_0_4294967295(1234567890);
     TEST_ASSERT_EQUAL_STRING("02010604086262621816D2FC403ED20296493ED20296493ED20296493ED2029649", btHome.getBytes().c_str());
-    
-    bool addResult = btHome.addCount_0_4294967295(1234567890);  // can't add more
+
+    bool addResult = btHome.addCount_0_4294967295(1234567890); // can't add more
     TEST_ASSERT_FALSE(addResult);
     TEST_ASSERT_EQUAL_STRING("02010604086262621816D2FC403ED20296493ED20296493ED20296493ED2029649", btHome.getBytes().c_str());
-} 
+}
 
 void test_packetLength()
 {
@@ -68,7 +64,7 @@ void test_packetLength()
     // D2FC40 = BTHome UUID
 
     Serial.println("Testing BtHome packet length...");
-    BtHomeV2Device btHome("sss","llll", false);
+    BtHomeV2Device btHome("sss", "llll", false);
     // 4 bytes
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0416D2FC40", btHome.getBytes().c_str());
     btHome.addCount_0_255(1);
@@ -83,7 +79,7 @@ void test_packetLength()
 void test_addTemperature()
 {
     Serial.println("Testing BtHome getBytes method...");
-    BtHomeV2Device btHome("sss","llll", false);
+    BtHomeV2Device btHome("sss", "llll", false);
     std::string result = btHome.getBytes();
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0416D2FC40", result.c_str());
     btHome.addTemperature(-22.0f, RANGE_127_RESOLUTION_1);
@@ -98,7 +94,7 @@ void test_addTemperature()
 
 void test_addDistance()
 {
-    BtHomeV2Device btHome("sss","llll", false);
+    BtHomeV2Device btHome("sss", "llll", false);
     std::string result = btHome.getBytes();
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0416D2FC40", result.c_str());
     btHome.addDistanceMetres(7.8);
@@ -109,7 +105,7 @@ void test_addDistance()
 
 void test_addCount_unsigned_integer()
 {
-    BtHomeV2Device btHome("sss","llll", false);
+    BtHomeV2Device btHome("sss", "llll", false);
 
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0416D2FC40", btHome.getBytes().c_str());
     bool success = btHome.addCount_0_255(96);
@@ -119,14 +115,13 @@ void test_addCount_unsigned_integer()
     btHome.addCount_0_65535(24585);
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0916D2FC4009603D0960", btHome.getBytes().c_str());
 
-     
     btHome.addCount_0_4294967295(1611213866);
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0E16D2FC4009603D09603E2A2C0960", btHome.getBytes().c_str());
 }
 
 void test_addCount_signed_integer()
 {
-    BtHomeV2Device btHome("sss","llll", false);
+    BtHomeV2Device btHome("sss", "llll", false);
     btHome.addCount_neg128_127(-22);
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0616D2FC4059EA", btHome.getBytes().c_str());
 
@@ -149,7 +144,7 @@ void setup()
 void loop()
 {
     delay(500);
-    
+
     RUN_TEST(test_completeNameLengthSwitchover);
     RUN_TEST(test_completeName);
     RUN_TEST(test_packetLength);
