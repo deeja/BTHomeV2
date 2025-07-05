91630f28258c4b8d6ff7e88906d8033bfe50fda1
dannjb <deeja@users.noreply.github.com>
2025-07-05 12:23:19 +1200

    Update with broken tests. Data needs changing on all tests

--- src/main.cpp
+++ src/main.cpp
@@ -2,49 +2,20 @@
 #include <BtHomeV2Device.h>
 #include "NimBLEDevice.h"
 
-// Overload for std::string (treat as raw bytes)
-void printBytes(const std::string &bytes)
-{
-  Serial.println(String(bytes.c_str()));
-}
-
 void setup()
 {
   Serial.begin(115200);
   delay(50);
-  Serial.println("Starting BT Home Example...");
+  Serial.println("Starting BTHomeV2-Arduino Example...");
 }
 bool buttonOn = false;
 
-void loop()
+void sendAdvertisement(uint8_t advertisementData[], size_t size)
 {
   NimBLEDevice::init("");
-  BtHomeV2Device btHome("aaa", "aaa", false);
-
-  // btHome.eventButton(Button_Event_Status_Hold_Press);
-  if (buttonOn)
-  {
-    btHome.eventButton(Button_Event_Status_Press);
-    btHome.eventButton(Button_Event_Status_Press);
-    btHome.eventButton(Button_Event_Status_Press);
-    btHome.eventDimmer(Dimmer_Event_Status_None, 0);
-
-  }
-  else
-  {
-     btHome.eventButton(Button_Event_Status_None);
-     btHome.eventButton(Button_Event_Status_None);
-     btHome.eventButton(Button_Event_Status_None);
-    btHome.eventDimmer(Dimmer_Event_Status_RotateLeft, 10);
-  }
-
-  buttonOn = !buttonOn;
-
-  uint8_t buffer[MAX_PAYLOAD_SIZE];
-  size_t size = btHome.getAdvertisementData(buffer);
   NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
   NimBLEAdvertisementData pAdvData = BLEAdvertisementData();
-  std::vector<uint8_t> data(buffer, buffer + size);
+  std::vector<uint8_t> data(advertisementData, advertisementData + size);
 
   pAdvData.addData(data);
   pAdvertising->setAdvertisementData(pAdvData);
@@ -53,8 +24,39 @@ void loop()
   pAdvertising->start();
   delay(1000);
   pAdvertising->stop();
+  Serial.println("Stopping advertising...");
+}
+
+void loop()
+{
+
+  // Short or long name sent based on packet size
+  BtHomeV2Device btHome("short_name", "My longer device name", false);
+
+  // Add humidity
+  btHome.addHumidityPercent_Resolution_0_01(60.55f);
+
+  // Set Battery State
+  for (size_t i = 0; i < 20; i++)
+  {
+    btHome.setBatteryState(BATTERY_STATE_NORMAL);
+  }
+
+  // Set battery percentage
+  btHome.addBatteryPercentage(35);
+
+  // Add Temperature
+  btHome.addTemperature_neg127_to_127_Resolution_1(-22.0f);
+
+  uint8_t advertisementData[MAX_ADVERTISEMENT_SIZE];
+  size_t size = btHome.getAdvertisementData(advertisementData);
+
+  Serial.print("Advertisement data size: ");
+  Serial.println(size);
+
+  // Set the advertisement data on BLE
+  sendAdvertisement(advertisementData, size); // <- this method isn't part of the library. Look at the examples
+  Serial.println("Advertising data sent.");
 
   delay(5000);
-
-  Serial.println("Advertising data sent.");
 }
--- test/test.cpp
+++ test/test.cpp
@@ -51,19 +51,10 @@ void test_completeNameLengthSwitchover()
 {
     Serial.println("Testing BtHome full local name...");
     BtHomeV2Device btHome("bbb", "aaaaaaaaaaaa", false);
-    TEST_ASSERT_EQUAL_STRING("0201060D096161616161616161616161610416D2FC40", getHexString(btHome).c_str());
+    TEST_ASSERT_EQUAL_STRING("0201060D0961616161616161616161616104086262620416D2FC40", getHexString(btHome).c_str());
     btHome.addCount_0_4294967295(1234567890);
-    TEST_ASSERT_EQUAL_STRING("0201060D096161616161616161616161610916D2FC403ED2029649", getHexString(btHome).c_str());
     btHome.addCount_0_4294967295(1234567890);
-    TEST_ASSERT_EQUAL_STRING("0201060D096161616161616161616161610E16D2FC403ED20296493ED2029649", getHexString(btHome).c_str());
-    btHome.addCount_0_4294967295(1234567890);
-    TEST_ASSERT_EQUAL_STRING("02010604086262621316D2FC403ED20296493ED20296493ED2029649", getHexString(btHome).c_str());
-    btHome.addCount_0_4294967295(1234567890);
-    TEST_ASSERT_EQUAL_STRING("02010604086262621816D2FC403ED20296493ED20296493ED20296493ED2029649", getHexString(btHome).c_str());
-
-    bool addResult = btHome.addCount_0_4294967295(1234567890); // can't add more
-    TEST_ASSERT_FALSE(addResult);
-    TEST_ASSERT_EQUAL_STRING("02010604086262621816D2FC403ED20296493ED20296493ED20296493ED2029649", getHexString(btHome).c_str());
+    TEST_ASSERT_EQUAL_STRING("02010604086262620916D2FC403ED2029649", getHexString(btHome).c_str());
 }
 
 void test_packetLength()
@@ -80,7 +71,7 @@ void test_packetLength()
     // D2FC40 = BTHome UUID
 
     Serial.println("Testing BtHome packet length...");
-    BtHomeV2Device btHome("sss", "llll", false);
+    BtHomeV2Device btHome("sss", "lllllllllllllllll", false);
     // 4 bytes
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0416D2FC40", getHexString(btHome).c_str());
     btHome.addCount_0_255(1);
@@ -95,21 +86,21 @@ void test_packetLength()
 void test_addTemperature()
 {
     Serial.println("Testing BtHome getBytes method...");
-    BtHomeV2Device btHome("sss", "llll", false);
+    BtHomeV2Device btHome("sss", "lllllllllllllllll", false);
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0416D2FC40", getHexString(btHome).c_str());
-    btHome.addTemperature(-22.0f, RANGE_127_RESOLUTION_1);
+    btHome.addTemperature_neg127_to_127_Resolution_1(-22.0f);
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0616D2FC4057EA", getHexString(btHome).c_str());
-    btHome.addTemperature(-7.7f, RANGE_44_RESOLUTION_0_35);
+    btHome.addTemperature_neg44_to_44_Resolution_0_35(-7.7f);
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0816D2FC4057EA58EA", getHexString(btHome).c_str());
-    btHome.addTemperature(25.06f, RANGE_327_RESOLUTION_0_01);
+    btHome.addTemperature_neg327_to_327_Resolution_0_01(25.06f);
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0B16D2FC4057EA58EA02CA09", getHexString(btHome).c_str());
-    btHome.addTemperature(27.3f, RANGE_3276_RESOLUTION_0_1);
+    btHome.addTemperature_neg3276_to_3276_Resolution_0_1(27.3f);
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0E16D2FC4057EA58EA02CA09451101", getHexString(btHome).c_str());
 }
 
 void test_addDistance()
 {
-    BtHomeV2Device btHome("sss", "llll", false);
+    BtHomeV2Device btHome("sss", "lllllllllllllllll", false);
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0416D2FC40", getHexString(btHome).c_str());
     btHome.addDistanceMetres(7.8);
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0716D2FC40414E00", getHexString(btHome).c_str());
@@ -119,17 +110,18 @@ void test_addDistance()
 
 void test_addHumidity()
 {
-    BtHomeV2Device btHome("sss", "llll", false);
+    BtHomeV2Device btHome("sss", "lllllllllllllllll", false);
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0416D2FC40", getHexString(btHome).c_str());
-    btHome.addHumidity_0_01(50.55f); // 41.4% humidity
+    btHome.addHumidityPercent_Resolution_0_01(50.55f); // 41.4% humidity
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0716D2FC4003BF13", getHexString(btHome).c_str());
-    btHome.addHumidity_0_1(35.0f); // 41.4% humidity
+    btHome.addHumidityPercent_Resolution_1(35.0f); // 41.4% humidity
+
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0916D2FC4003BF132E23", getHexString(btHome).c_str());
 }
 
 void test_addCount_unsigned_integer()
 {
-    BtHomeV2Device btHome("sss", "llll", false);
+    BtHomeV2Device btHome("sss", "lllllllllllllllll", false);
 
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0416D2FC40", getHexString(btHome).c_str());
     bool success = btHome.addCount_0_255(96);
@@ -145,7 +137,7 @@ void test_addCount_unsigned_integer()
 
 void test_addCount_signed_integer()
 {
-    BtHomeV2Device btHome("sss", "llll", false);
+    BtHomeV2Device btHome("sss", "lllllllllllllllll", false);
     btHome.addCount_neg128_127(-22);
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0616D2FC4059EA", getHexString(btHome).c_str());
 
@@ -159,18 +151,18 @@ void test_addCount_signed_integer()
 void test_state_battery()
 {
     // 0x15 	battery 	uint8 (1 byte) 	1501 	0 (False = Normal) 1 (True = Low)
-    BtHomeV2Device btHome("sss", "llll", false);
+    BtHomeV2Device btHome("sss", "lllllllllllllllll", false);
     btHome.setBatteryState(BATTERY_STATE_NORMAL);
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0616D2FC401500", getHexString(btHome).c_str());
 
-    BtHomeV2Device btHome2("sss", "llll", false);
+    BtHomeV2Device btHome2("sss", "lllllllllllllllll", false);
     btHome2.setBatteryState(BATTERY_STATE_LOW);
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0616D2FC401501", getHexString(btHome2).c_str());
 }
 
 void test_addText()
 {
-    BtHomeV2Device btHome("sss", "llll", false);
+    BtHomeV2Device btHome("sss", "lllllllllllllllll", false);
     btHome.addText("Hello World!");
 
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C1216D2FC40530C48656C6C6F20576F726C6421", getHexString(btHome).c_str());
@@ -178,16 +170,17 @@ void test_addText()
 
 void test_addTime()
 {
-    BtHomeV2Device btHome("sss", "llll", false);
+    BtHomeV2Device btHome("sss", "lllllllllllllllll", false);
 
     uint64_t secondsSinceEpoch = 1684093277;
     btHome.addTime(secondsSinceEpoch);
+
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0916D2FC40505D396164", getHexString(btHome).c_str());
 }
 
 void test_addRaw()
 {
-    BtHomeV2Device btHome("sss", "llll", false);
+    BtHomeV2Device btHome("sss", "lllllllllllllllll", false);
     uint8_t array[4] = {0x00, 0x01, 0x02, 0x03};
     btHome.addRaw(array, 4);
 
@@ -196,24 +189,61 @@ void test_addRaw()
 
 void test_eventButton()
 {
-    BtHomeV2Device btHome("sss", "llll", false);
-    btHome.eventButton(Button_Event_Status_Double_Press);
+    BtHomeV2Device btHome("sss", "lllllllllllllllll", false);
+    btHome.setButtonEvent(Button_Event_Status_Double_Press);
 
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0616D2FC403A02", getHexString(btHome).c_str());
-    btHome.eventButton(Button_Event_Status_Long_Triple_Press);
+    btHome.setButtonEvent(Button_Event_Status_Long_Triple_Press);
 
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0816D2FC403A023A06", getHexString(btHome).c_str());
 }
 
 void test_eventDimmer()
 {
-    BtHomeV2Device btHome("sss", "llll", false);
-    btHome.eventDimmer(Dimmer_Event_Status_RotateLeft, 3);
+    BtHomeV2Device btHome("sss", "lllllllllllllllll", false);
+    btHome.setDimmerEvent(Dimmer_Event_Status_RotateLeft, 3);
 
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0716D2FC403C0103", getHexString(btHome).c_str());
-    btHome.eventDimmer(Dimmer_Event_Status_None, 120);
+    btHome.setDimmerEvent(Dimmer_Event_Status_None, 120);
     TEST_ASSERT_EQUAL_STRING("02010605096C6C6C6C0A16D2FC403C01033C0078", getHexString(btHome).c_str());
 }
+
+void test_maxPacketSize()
+{
+    BtHomeV2Device btHome("sss", "lllllllllllllllll", false);
+
+    uint8_t advertisementData[MAX_ADVERTISEMENT_SIZE];
+
+    for (size_t i = 0; i < 50; i++)
+    {
+        btHome.setColdState(Cold_Sensor_Status_Cold);
+        uint8_t size = btHome.getAdvertisementData(advertisementData);
+        char msg[32];
+        snprintf(msg, sizeof(msg), "Iteration %zu, size: %u", i, size);
+        TEST_ASSERT_LESS_THAN_MESSAGE(MAX_ADVERTISEMENT_SIZE, size, msg);
+    }
+}
+
+void test_shortAndCompleteNames()
+{
+    // under limits
+    BtHomeV2Device btHome("aaaaaaaaa", "lllllllll", false);
+    TEST_ASSERT_EQUAL_STRING("0201060A096C6C6C6C6C6C6C6C6C0A086161616161616161610416D2FC40", getHexString(btHome).c_str());
+
+    // on limits
+    BtHomeV2Device btHome2("aaa", "ffffffffffffffffffff", false);
+    TEST_ASSERT_EQUAL_STRING("0201061509666666666666666666666666666666666666666604086161610416D2FC40", getHexString(btHome2).c_str());
+    BtHomeV2Device btHome2b("aaaaaaaaaa", "ffffffff", false);
+    TEST_ASSERT_EQUAL_STRING("020106090966666666666666660B08616161616161616161610416D2FC40", getHexString(btHome2b).c_str());
+
+
+    // over limits
+    BtHomeV2Device btHome3("aaa", "ffffffffffffffffffffffff", false);
+    TEST_ASSERT_EQUAL_STRING("0201061509666666666666666666666666666666666666666604086161610416D2FC40", getHexString(btHome3).c_str());
+    BtHomeV2Device btHome3b("aaaaaaaaaaaaaaa", "ffffffff", false);
+    TEST_ASSERT_EQUAL_STRING("020106090966666666666666660B08616161616161616161610416D2FC40", getHexString(btHome3b).c_str());
+}
+
 void setup()
 {
     // NOTE!!! Wait for >2 secs
@@ -226,6 +256,9 @@ void setup()
 void loop()
 {
     delay(500);
+
+    RUN_TEST(test_shortAndCompleteNames);
+    RUN_TEST(test_maxPacketSize);
 
     RUN_TEST(test_eventDimmer);
     RUN_TEST(test_eventButton);
